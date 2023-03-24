03d77bff67b159ef0ee08f353b0660edf7814886
Innders <49156310+Innders@users.noreply.github.com>
2023-03-24 14:27:46 +0000

    cleanup

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -11,141 +11,126 @@
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'AYON',
-  tagline: 'The most robust open-source pipeline for studios and remote teams',
-  url: 'https://ayon.ynput.io',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+    title: "AYON",
+    tagline:
+        "The most robust open-source pipeline for studios and remote teams",
+    url: "https://ayon.ynput.io",
+    baseUrl: "/",
+    onBrokenLinks: "throw",
+    onBrokenMarkdownLinks: "warn",
+    favicon: "img/favicon.ico",
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'ynput', // Usually your GitHub org/user name.
-  projectName: 'ayon-documentation', // Usually your repo name.
+    // GitHub pages deployment config.
+    // If you aren't using GitHub pages, you don't need these.
+    organizationName: "ynput", // Usually your GitHub org/user name.
+    projectName: "ayon-documentation", // Usually your repo name.
 
-  presets: [
-    [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/ynput/OpenPype/tree/develop/website',
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      }),
+    presets: [
+        [
+            "classic",
+            /** @type {import('@docusaurus/preset-classic').Options} */
+            ({
+                docs: {
+                    sidebarPath: require.resolve("./sidebars.js"),
+                    // Please change this to your repo.
+                    // Remove this to remove the "edit this page" links.
+                    editUrl:
+                        "https://github.com/ynput/OpenPype/tree/develop/website",
+                },
+                theme: {
+                    customCss: require.resolve("./src/css/custom.css"),
+                },
+            }),
+        ],
     ],
-  ],
 
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
-        title: 'AYON',
-        logo: {
-          alt: 'AYON Logo',
-          src: 'img/logo.svg',
-        },
-        items: [
-          {
-            to: '/features',
-            label: 'Features',
-            position: 'left',
-          },
-          {
-            to: 'docs/artist_getting_started',
-            label: 'User Docs',
-            position: 'left',
-          },
-          {
-            to: 'docs/system_introduction',
-            label: 'Admin Docs',
-            position: 'left',
-          },
-          {
-            to: 'docs/dev_introduction',
-            label: 'Dev Docs',
-            position: 'left',
-          },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Pages',
-            items: [
-              {
-                label: 'Features',
-                to: '/features',
-              },
-              {
-                label: 'User Docs',
-                to: '/docs/artist_getting_started',
-              },
-              {
-                label: 'Admin Docs',
-                to: '/docs/system_introduction',
-              },
-              {
-                label: 'Dev Docs',
-                to: '/docs/dev_introduction',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Discord',
-                href: 'https://discord.gg/ynput',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/ynput/OpenPype',
-              },
-              {
-                label: 'LinkedIn',
-                href: 'https://www.linkedin.com/company/ynput',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/ynput_io',
-              },
-            ],
-          },
-          // {
-          //   title: 'Legal',
-          //   // Please do not remove the privacy and terms, it's a legal requirement.
-          //   items: [
-          //     {
-          //       label: 'Privacy',
-          //       href: 'https://opensource.fb.com/legal/privacy/',
-          //     },
-          //     {
-          //       label: 'Terms',
-          //       href: 'https://opensource.fb.com/legal/terms/',
-          //     },
-          //     {
-          //       label: 'Data Policy',
-          //       href: 'https://opensource.fb.com/legal/data-policy/',
-          //     },
-          //     {
-          //       label: 'Cookie Policy',
-          //       href: 'https://opensource.fb.com/legal/cookie-policy/',
-          //     },
-          //   ],
-          // },
-        ],
-        // Please do not remove the credits, help to publicize Docusaurus :)
-        copyright: `Copyright © ${new Date().getFullYear()} Ynput`,
-      },
-    }),
+    themeConfig:
+        /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+        ({
+            navbar: {
+                title: "AYON",
+                logo: {
+                    alt: "AYON Logo",
+                    src: "img/logo.svg",
+                },
+                items: [
+                    {
+                        to: "/features",
+                        label: "Features",
+                        position: "left",
+                    },
+                    {
+                        to: "docs/artist_getting_started",
+                        label: "User Docs",
+                        position: "left",
+                    },
+                    {
+                        to: "docs/system_introduction",
+                        label: "Admin Docs",
+                        position: "left",
+                    },
+                    {
+                        to: "docs/dev_introduction",
+                        label: "Dev Docs",
+                        position: "left",
+                    },
+                    {
+                        href: "https://github.com/pypeclub",
+                        label: "GitHub",
+                        position: "right",
+                    },
+                ],
+            },
+            footer: {
+                style: "dark",
+                links: [
+                    {
+                        title: "Pages",
+                        items: [
+                            {
+                                label: "Features",
+                                to: "/features",
+                            },
+                            {
+                                label: "User Docs",
+                                to: "/docs/artist_getting_started",
+                            },
+                            {
+                                label: "Admin Docs",
+                                to: "/docs/system_introduction",
+                            },
+                            {
+                                label: "Dev Docs",
+                                to: "/docs/dev_introduction",
+                            },
+                        ],
+                    },
+                    {
+                        title: "Community",
+                        items: [
+                            {
+                                label: "Discord",
+                                href: "https://discord.gg/ynput",
+                            },
+                            {
+                                label: "GitHub",
+                                href: "https://github.com/ynput/OpenPype",
+                            },
+                            {
+                                label: "LinkedIn",
+                                href: "https://www.linkedin.com/company/ynput",
+                            },
+                            {
+                                label: "Twitter",
+                                href: "https://twitter.com/ynput_io",
+                            },
+                        ],
+                    },
+                ],
+                // Please do not remove the credits, help to publicize Docusaurus :)
+                copyright: `Copyright © ${new Date().getFullYear()} Ynput`,
+            },
+        }),
 };
 
 module.exports = config;
--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -7,148 +7,158 @@
  * @format
  */
 
-import React from 'react';
-import clsx from 'clsx';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import useBaseUrl from '@docusaurus/useBaseUrl';
-import styles from './styles.module.css';
+import React from "react";
+import clsx from "clsx";
+import Layout from "@theme/Layout";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+import styles from "./styles.module.css";
 
 const features = [
-  {
-    title: 'Battle tested',
-    //imageUrl: '',
-    description: (
-      <>
-        Designed, used, broken-in and validated in collaboration with many
-        studios, who's artist have used it on projects ranging from commercials,
-        to features.
-      </>
-    ),
-  },
-  {
-    title: 'Supported',
-    //imageUrl: '',
-    description: (
-      <>
-        AYON is developed and maintained by ynput.io, a full-time, dedicated
-        team of industry professionals, providing support and training to
-        studios and artists.
-      </>
-    ),
-  },
-  {
-    title: 'Extensible',
-    //imageUrl: '',
-    description: (
-      <>
-        Project needs differ, clients differ and studios differ. AYON is
-        designed to fit into your workflow and bend to your will. If a feature
-        is missing, it can most probably be added.
-      </>
-    ),
-  },
-  {
-    title: 'Focused',
-    //imageUrl: '',
-    description: (
-      <>
-        All AYON features have been added to solve specific needs during it's
-        use in production. If something is obsolete, it is carefully deprecated,
-        to keep the codebase lean and easier to maintain.
-      </>
-    ),
-  },
+    {
+        title: "Battle tested",
+        //imageUrl: '',
+        description: (
+            <>
+                Designed, used, broken-in and validated in collaboration with
+                many studios, who's artist have used it on projects ranging from
+                commercials, to features.
+            </>
+        ),
+    },
+    {
+        title: "Supported",
+        //imageUrl: '',
+        description: (
+            <>
+                AYON is developed and maintained by ynput.io, a full-time,
+                dedicated team of industry professionals, providing support and
+                training to studios and artists.
+            </>
+        ),
+    },
+    {
+        title: "Extensible",
+        //imageUrl: '',
+        description: (
+            <>
+                Project needs differ, clients differ and studios differ. AYON is
+                designed to fit into your workflow and bend to your will. If a
+                feature is missing, it can most probably be added.
+            </>
+        ),
+    },
+    {
+        title: "Focused",
+        //imageUrl: '',
+        description: (
+            <>
+                All AYON features have been added to solve specific needs during
+                it's use in production. If something is obsolete, it is
+                carefully deprecated, to keep the codebase lean and easier to
+                maintain.
+            </>
+        ),
+    },
 ];
 
-function Feature({imageUrl, title, description}) {
-  const imgUrl = useBaseUrl(imageUrl);
-  return (
-    <div className={clsx('col col--3', styles.feature)}>
-      {imgUrl && (
-        <div className="text--center">
-          <img className={styles.featureImage} src={imgUrl} alt={title} />
+function Feature({ imageUrl, title, description }) {
+    const imgUrl = useBaseUrl(imageUrl);
+    return (
+        <div className={clsx("col col--3", styles.feature)}>
+            {imgUrl && (
+                <div className="text--center">
+                    <img
+                        className={styles.featureImage}
+                        src={imgUrl}
+                        alt={title}
+                    />
+                </div>
+            )}
+            <h3>{title}</h3>
+            <p>{description}</p>
         </div>
-      )}
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </div>
-  );
+    );
 }
 
 export default function Home() {
-  const context = useDocusaurusContext();
-  const {siteConfig = {}} = context;
-  return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <header className={clsx('hero hero--primary', styles.heroBanner)}>
-        <div className="container">
-          <h1 className="hero__title">{siteConfig.title}</h1>
-          <p className="hero__subtitle">{siteConfig.tagline}</p>
-          <div className={styles.buttons}>
-            <Link
-              className={clsx(
-                'button button--outline button--secondary button--md',
-                styles.getStarted,
-              )}
-              to={'https://github.com/pypeclub/ayon-documentation'}>
-              Try AYON
-            </Link>
-            <Link
-              className={clsx(
-                'button button--outline button--secondary button--md',
-                styles.getStarted,
-              )}
-              to={useBaseUrl('docs/')}>
-              Contribute
-            </Link>
-          </div>
-        </div>
-      </header>
+    const context = useDocusaurusContext();
+    const { siteConfig = {} } = context;
+    return (
+        <Layout title={siteConfig.title} description={siteConfig.tagline}>
+            <header className={clsx("hero hero--primary", styles.heroBanner)}>
+                <div className="container">
+                    <div className={styles.title}>
+                        <img
+                            src="img/logo.svg"
+                            alt="AYON logo"
+                            width="100"
+                            height="100"
+                        />
+                        <h1 className={`${styles.projectTitle} hero__title`}>
+                            {siteConfig.title}
+                        </h1>
+                    </div>
+                    <p className="hero__subtitle">{siteConfig.tagline}</p>
+                    <div className={styles.buttons}>
+                        <Link
+                            className={clsx(
+                                "button button--secondary button--lg",
+                                styles.getStarted
+                            )}
+                            to={useBaseUrl("introduction/getting-started")}
+                        >
+                            Get Started
+                        </Link>
+                    </div>
+                </div>
+            </header>
 
-      <main>
-        {features && features.length > 0 && (
-          <section className={styles.features}>
-            <div className="container">
-              <div className="row">
-                {features.map(({title, imageUrl, description}) => (
-                  <Feature
-                    key={title}
-                    title={title}
-                    imageUrl={imageUrl}
-                    description={description}
-                  />
-                ))}
-              </div>
-            </div>
-          </section>
-        )}
+            <main>
+                {features && features.length > 0 && (
+                    <section className={styles.features}>
+                        <div className="container">
+                            <div className="row">
+                                {features.map(
+                                    ({ title, imageUrl, description }) => (
+                                        <Feature
+                                            key={title}
+                                            title={title}
+                                            imageUrl={imageUrl}
+                                            description={description}
+                                        />
+                                    )
+                                )}
+                            </div>
+                        </div>
+                    </section>
+                )}
 
-        <section className={(styles.features, 'darkBackground')}>
-          <div className="container">
-            <div className="row">
-              <div className="col col--6">
-                <img src="" />
-              </div>
-              <div className="col col--6">
-                <h2>Join the creative pipeline revolution</h2>
-                <p>
-                  AYON is for anyone that wants to make their creative studio
-                  scalable to take on more projects with a pipeline that is
-                  production-proven and continuously improving in-line with
-                  industry demands. We are funded by the industry to strengthen
-                  the industry. AYON opens the door for your creative team to
-                  take on new projects, giving a scalable platform to grow your
-                  studio.
-                </p>
-              </div>
-            </div>
-          </div>
-        </section>
-      </main>
-    </Layout>
-  );
+                <section className={(styles.features, "darkBackground")}>
+                    <div className="container">
+                        <div className="row">
+                            <div className="col col--6">
+                                <img src="" />
+                            </div>
+                            <div className="col col--6">
+                                <h2>Join the creative pipeline revolution</h2>
+                                <p>
+                                    AYON is for anyone that wants to make their
+                                    creative studio scalable to take on more
+                                    projects with a pipeline that is
+                                    production-proven and continuously improving
+                                    in-line with industry demands. We are funded
+                                    by the industry to strengthen the industry.
+                                    AYON opens the door for your creative team
+                                    to take on new projects, giving a scalable
+                                    platform to grow your studio.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </main>
+        </Layout>
+    );
 }
