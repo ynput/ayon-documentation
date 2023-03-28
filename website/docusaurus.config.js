f620f46b8aba7f44bda60bdd3d91a37f2f146168
Innders <49156310+Innders@users.noreply.github.com>
2023-03-28 10:44:29 +0100

    wip: docusaurus showcase

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -145,6 +145,21 @@ const config = {
                 ],
             },
         }),
+
+    plugins: [
+        [
+            "ideal-image",
+            /** @type {import('@docusaurus/plugin-ideal-image').PluginOptions} */
+            ({
+                quality: 70,
+                max: 1030,
+                min: 640,
+                steps: 2,
+                // Use false to debug, but it incurs huge perf costs
+                disableInDev: true,
+            }),
+        ],
+    ],
 };
 
 module.exports = config;
