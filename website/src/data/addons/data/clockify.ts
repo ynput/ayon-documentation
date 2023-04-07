fa7cb8bb4e066676d7f869ecc40fb54e784b8be1
Innders <49156310+Innders@users.noreply.github.com>
2023-04-07 09:17:56 +0100

    data migration

---
+++ website/src/data/addons/data/clockify.ts
@@ -0,0 +1,14 @@
+import { type Addon } from "../types";
+
+const addon: Addon = {
+    title: "Clockify",
+    description:
+        "Clockify is a free time tracking software that helps you track your time across projects and tasks.",
+    docs: {
+        admin: "module_clockify",
+    },
+    icon: "clockify-icon.png",
+    github: "https://github.com/ynput/ayon-addons/tree/main/clockify/dev",
+};
+
+export default addon;
