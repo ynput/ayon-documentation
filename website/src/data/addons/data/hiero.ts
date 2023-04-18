b5a4e1df5bbc9f2b565fe2979613ab6867c6f07e
Jakub Jezek <jakubjezek001@gmail.com>
2023-04-18 16:30:42 +0200

    hiero families and description

--- website/src/data/addons/data/hiero.ts
+++ website/src/data/addons/data/hiero.ts
@@ -3,7 +3,7 @@ import { type Addon } from "../types";
 const addon: Addon = {
     title: "Hiero",
     description:
-        "Hiero is a versatile VFX tool for artists and teams. It helps you review, edit, and conform shots.",
+        "Hiero and NukeStudio is a versatile VFX tool for artists and teams. It helps you review, edit, and conform shots.",
     preview: "hiero.png",
     features: [
         "workfiles",
@@ -12,17 +12,14 @@ const addon: Addon = {
         "sceneManager",
         "libraryLoader",
         "reviewables",
-        "colorManaged",
-        "localRendering",
+        "colorManaged"
     ],
     families: [
-        "render",
+        "audio",
         "plate",
         "review",
-        "lut",
-        "image",
-        "nukeNodes",
-        "gizmo",
+        "frame",
+        "effect",
         "workfile",
     ],
     docs: {
