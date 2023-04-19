9123e9f7ec619b474704650e92ffe64e6c573cee
Kayla Man <moonyuet2008@hotmail.com>
2023-04-19 18:24:59 +0800

    user docs of the 3dsmax addon is being redirected correctly

--- website/src/data/addons/data/3dsmax.ts
+++ website/src/data/addons/data/3dsmax.ts
@@ -25,8 +25,8 @@ const addon: Addon = {
         "maxScene",
     ],
     docs: {
-        user: "artist_hosts_houdini",
-        admin: "admin_hosts_houdini",
+        user: "artist_hosts_3dsmax",
+        admin: "admin_hosts_Houdini",
     },
     github: "https://github.com/ynput/OpenPype/tree/develop/openpype/hosts/houdini",
 };
