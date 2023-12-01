a20debcfc6a7b2336f321e6810bcc9b245bfbdfd
MustafaJafar <mustafataherzaky@outlook.com>
2023-12-01 14:14:04 +0200

    add badge to substance painter card and admin doc

--- website/src/data/addons/data/substancepainter.ts
+++ website/src/data/addons/data/substancepainter.ts
@@ -1,10 +1,12 @@
 import { type Addon } from "../types";
+import versions from "@site/docs/assets/json/Ayon_addons_version.json";
 
 const addon: Addon = {
     title: "Substance Painter",
     description:
         "Substance 3D Painter is a 3D painting software that allows users to texture and add materials directly to 3D meshes in real-time.",
     icon: "substance-painter-icon.png",
+    badge: versions.SubstancePainter_Badge,
     features: [
         "workfiles",
         "loader",
