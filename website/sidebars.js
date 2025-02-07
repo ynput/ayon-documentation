3b5e8677a039ec7d6adc92e267682d9b965874a8
MustafaJafar <mustafataherzaky@outlook.com>
2025-02-07 14:23:20 +0200

    add zbrush docs to sidebar

--- website/sidebars.js
+++ website/sidebars.js
@@ -120,6 +120,7 @@ module.exports = {
                     items: ["addon_aquarium_artist", "addon_aquarium_manager"],
                 },
                 "addon_cinema4d_artist",
+                "addon_zbrush_artist"
             ].sort(sorted),
         },
         {
@@ -267,6 +268,7 @@ module.exports = {
                     ]
                 },
                 "addon_cinema4d_admin",
+                "addon_zbrush_admin"
             ].sort(sorted),
         },
         "admin_releases",
