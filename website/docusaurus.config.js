89f5661a900197c0ba5bd0dee7cd182c1adf325e
Innders <49156310+Innders@users.noreply.github.com>
2025-03-31 13:19:01 +0100

    chore: migrate plausible domain

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -209,7 +209,7 @@ const config = {
         {
             src: "https://plausible.io/js/script.js",
             defer: true,
-            "data-domain": "ayon.ynput.io",
+            "data-domain": "docs.ayon.dev",
         },
     ],
 };
